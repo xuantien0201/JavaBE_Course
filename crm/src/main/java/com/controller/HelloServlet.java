b75104d33640503f6e0fc51c2f63577b6e82f69d
xuantien0201 <xtien1812@gmail.com>
2025-09-23 08:09:58 +0700

    vong doi cua servlet

--- crm/src/main/java/com/controller/HelloServlet.java
+++ crm/src/main/java/com/controller/HelloServlet.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
 public class HelloServlet extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("HelloServlet initialized");
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		resp.getWriter().append("Hello, World!");
+		req.getRequestDispatcher("/hello.jsp").forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPost method called");
+
 	}
 }
---
+++ crm/src/main/webapp/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2><%= "Hi Im Tien Tran" %></h2>
+</body>
+</html>
