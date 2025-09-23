49131509e00c15558c3ce2bc2ede8a33e1f928ae
xuantien0201 <xtien1812@gmail.com>
2025-09-23 16:44:12 +0700

    1st

--- crm/src/main/java/com/controller/Login.java
+++ crm/src/main/java/com/controller/Login.java
@@ -9,21 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("HelloServlet initialized");
+		
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// getRequestDispatcher() method cho phép chuyển tiếp yêu cầu đến một tài nguyên khác (như JSP, HTML, hoặc một servlet khác) trên server.
-		req.getRequestDispatcher("/hello.jsp").forward(req, resp);
+		req.getRequestDispatcher("/login.jsp").forward(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doPost method called");
-
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		if(username.equals("admin") && password.equals("123456")) {
+			resp.sendRedirect( req.getContextPath() + "/hello");
+		}else {
+			req.getRequestDispatcher("/login.jsp").forward(req, resp);
+		}
 	}
+
 }
---
+++ crm/src/main/webapp/login.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<form action = "http://localhost:8080/crm/login" method = "post">
+		<input type = "text" name = "username" placeholder = "User name"/>
+		<input type = "password" name = "password" placeholder = "Password"/>
+		<button>Login</button>
+	</form>
+</body>
+</html>
